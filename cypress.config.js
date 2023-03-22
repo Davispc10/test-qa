bf36767e596f2941aedd21c1d4e8e695b5d3ef83
Leo Costa <leolpc21@hotmail.com>
2023-03-22 09:48:04 -0300

    implementando page objects e fixture

--- cypress.config.js
+++ cypress.config.js
@@ -1,7 +1,7 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  defaultCommandTimeout: 15000,
+  chromeWebSecurity: false,
   e2e: {
     baseUrl: 'https://www.saucedemo.com',
     defaultCommandTimeout: 15000,
--- cypress/e2e/login_test.cy.js
+++ cypress/e2e/login_test.cy.js
@@ -1,45 +1,55 @@
 /// <reference types="cypress" />
+import loginPage from "../support/pages/login"
 
 describe('Validar login', () => {
 
-  var username = 'user_incorreto'
-  var password = 'pass_incorreto'
-
-  var user = {
-    username: 'standard_user',
-    password: 'secret_sauce'
+  var messageError = {
+    notMatch: 'Epic sadface: Username and password do not match any user in this service',
+    userEmpty: 'Epic sadface: Username is required',
+    passwordEmpty: 'Epic sadface: Password is required'
   }
 
-  var messagemError = 'Epic sadface: Username and password do not match any user in this service'
-
   beforeEach(function () {
+    cy.fixture('login').then(function (login) {
+      this.valid = login.userValid
+      this.invalid = login.userInvalid
+    })
     cy.visit('/')
   })
 
   context('Validar login incorreto', () => {
 
-    it('username inválido', () => {
-      cy.get('#user-name').type(username)
-      cy.get('#password').type(user.password)
-      cy.get('#login-button').click()
-      cy.get('[data-test="error"]').should('contain.text', messagemError)
+    it('username inválido', function () {
+      loginPage.form(this.invalid.username, this.valid.password)
+      loginPage.submit()
+      loginPage.messageError(messageError.notMatch)
     });
 
-    it('password inválido', () => {
-      cy.get('#user-name').type(user.username)
-      cy.get('#password').type(password)
-      cy.get('#login-button').click()
-      cy.get('[data-test="error"]').should('contain.text', messagemError)
+    it('password inválido', function () {
+      loginPage.form(this.valid.username, this.invalid.password)
+      loginPage.submit()
+      loginPage.messageError(messageError.notMatch)
+    });
+
+    it('username vazio', function () {
+      loginPage.formPassword(this.valid.password)
+      loginPage.submit()
+      loginPage.messageError(messageError.userEmpty)
+    });
+
+    it('password vazio', function () {
+      loginPage.formUsername(this.valid.username)
+      loginPage.submit()
+      loginPage.messageError(messageError.passwordEmpty)
     });
 
   })
 
   context('Validar login correto', () => {
 
-    it('login válido', () => {
-      cy.get('#user-name').type(user.username)
-      cy.get('#password').type(user.password)
-      cy.get('#login-button').click()
+    it('login válido', function () {
+      loginPage.form(this.valid.username, this.valid.password)
+      loginPage.submit()
     });
 
   })
